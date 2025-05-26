87a489bd4857228b431adafebcca0c1b73047e2a
mathe-man <mathieumousset6@gmail.com>
2025-05-26 18:11:52 +0300

    Add cmake list and Ansi escape code for window

--- DrimApp/main.cpp
+++ DrimApp/main.cpp
@@ -15,10 +15,30 @@
 #include "File.h"
 #include "Debug.h";
 
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void EnableAnsiEscapeCodes() {
+#ifdef _WIN32
+    // Sur Windows, il faut activer les séquences ANSI dans la console
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) return;
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) return;
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+
+#endif
+}
 //	<<>>
 
 
 int main()
 {
+    EnableAnsiEscapeCodes();
 	Debug::DebugEditor();
 }
